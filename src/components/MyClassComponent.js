0c955d6f35f3b784e1f791b95254e926d6c0fd60
ANKU <ankitadixit1122@gmail.com>
2023-11-07 23:29:02 -0600

    Assignment 2- React

---
+++ src/components/MyClassComponent.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+class MyClassComponent extends Component{
+    render(){
+        return(
+            <>
+            <h1>My City is {this.props.myCity}</h1>
+            </>
+        )
+    }
+    
+}
+
+export default MyClassComponent
+
+
+
+/* only one componnet is exported as default */
---
+++ src/components/MyComponent.js
@@ -0,0 +1,36 @@
+import { Component } from "react";
+
+class MyComponent extends Component{
+
+
+    state ={
+        value:''  // this value will be update everytime
+    };
+
+
+    getValue =(event) =>{
+        console.log("Event: ",event.target.value);
+        this.setState({ value: event.target.value});
+
+    }
+
+    handlesubmit =(event) =>{
+         event.preventDefault();                       // stop the browser from default behaviour of refreshing everytime(reloading)
+        const value =this.state.value;   // onClick submit this will get updated value from state
+    }
+
+
+    render(){
+        console.log('State: ',this.state);
+    return(
+        <>  
+            <h1>My Name is {this.state.value}</h1>
+            <h1>Enter your name</h1><br></br>
+            <input type='text' onChange = {this.getValue}/>
+            <button onClick={this.handlesubmit}>submit</button>  
+        </>
+        )
+    }    
+}
+
+export default MyComponent;
