50d6a034c1946202a766556261f87916dcac1690
ANKU <ankitadixit1122@gmail.com>
2023-11-17 14:44:18 -0600

    changes2

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import MyApp from './App';
 import reportWebVitals from './reportWebVitals';
-import MyClassComponent from './components/MyClassComponent';
-import Counter from './components/Counter';
-import CalculateSavings from './components/CalculateSavings';
-import CalculateSavingUsingClass from './components/CalculateSavingUsingClass';
-
-
+import HomeRoute from './components/router/HomeRoute';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MyApp/>
-    {/* <CalculateSavings/> */}
-    {/* <CalculateSavingUsingClass/> */}
-    
-    </React.StrictMode>
+    <HomeRoute/>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
-
 reportWebVitals();
+
+
+
+
+
+
+
